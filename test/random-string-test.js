f5194596d2be27300a21768fe01288f4c02a689e
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-11-18 22:07:10 +0100

    [test minor] Change `assert.length` to `assert.lengthOf`
    `vows@0.5.12` changes syntax from `assert.length` to `assert.lengthOf`.

--- test/random-string-test.js
+++ test/random-string-test.js
@@ -17,7 +17,7 @@ vows.describe('utile/randomString').addBatch({
       },
       "should return 16 characters that are actually random by default": function (random) {
         assert.isString(random);
-        assert.length(random, 16);
+        assert.lengthOf(random, 16);
         assert.notEqual(random, utile.randomString());
       },
       "when you can asked for different length strings": {
@@ -26,11 +26,11 @@ vows.describe('utile/randomString').addBatch({
         },
         "where they actually are of length 4, 128": function (strings) {
           assert.isArray(strings);
-          assert.length(strings,2);
+          assert.lengthOf(strings,2);
           assert.isString(strings[0]);
           assert.isString(strings[1]);
-          assert.length(strings[0], 4);
-          assert.length(strings[1], 128);
+          assert.lengthOf(strings[0], 4);
+          assert.lengthOf(strings[1], 128);
           assert.notEqual(strings[0], strings[1].substr(0,4));
         }
       }
