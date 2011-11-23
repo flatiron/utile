ca984cee3d1eac3b970c118a05cedf13689e34af
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-11-23 19:58:02 +0100

    [test] Test `requireDirectory`

---
+++ test/require-directory-test.js
@@ -0,0 +1,26 @@
+var assert = require('assert'),
+    path = require('path'),
+    vows = require('vows'),
+    utile = require('../');
+
+var requireFixtures = path.join(__dirname, 'fixtures', 'require-directory');
+
+vows.describe('utile/require-directory').addBatch({
+  'When using utile': {
+    'the `requireDirectory()` function': {
+      topic: utile.requireDirectory(requireFixtures),
+      'should contain all wanted modules': function (obj) {
+        assert.isObject(obj);
+        assert.deepEqual(obj, {
+          directory: {
+            me: 'directory/index.js'
+          },
+          helloWorld: {
+            me: 'helloWorld.js'
+          }
+        });
+      }
+    }
+  }
+}).export(module);
+
