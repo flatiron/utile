a00e0663cdaba944bc6ffb26475676455b9878d9
yawn <anonymous@email.com>
2012-04-05 17:57:27 +0200

    [api] Mixin setters as well

--- test/utile-test.js
+++ test/utile-test.js
@@ -27,6 +27,12 @@ obj2 = {
 obj2.__defineGetter__('bazz', function () {
   return 'bazz';
 });
+obj2.__defineSetter__('bazz', function() {
+  return 'bazz';
+});
+obj2.__defineSetter__('wat', function () {
+  return 'wat';
+});
  
 vows.describe('utile').addBatch({
   "When using utile": {
@@ -49,6 +55,8 @@ vows.describe('utile').addBatch({
       assert.isTrue(mixed.baz);
       assert.isString(mixed.buzz);
       assert.isTrue(!!mixed.__lookupGetter__('bazz'));
+      assert.isTrue(!!mixed.__lookupSetter__('bazz'));
+      assert.isTrue(!!mixed.__lookupSetter__('wat'));
       assert.isString(mixed.bazz);
     },
     "the clone() method": function () {
